1f4bebcb094dc159b403cd8f5e7542cdb4dfda49
kimheang-seavminh <kimheang@niph.org.kh>
2025-07-10 20:31:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World! Production-red Pod nginx-red New Name</p>'; ?>
+        <?php echo '<p>Hello World! Sopheak Cheak !</p>'; ?>
     </body>
 </html>
