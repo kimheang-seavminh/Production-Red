e995cd9bcf7e80fc8c14ef6b54a6e63ae20e05cf
kimheang-seavminh <kimheang@niph.org.kh>
2025-07-10 10:53:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World kimheang 07-10-2025</p>'; ?>
+        <?php echo '<p>Hello World kimheang 07-10-2025 Edit from Gitea Kimheang-seavminh</p>'; ?>
     </body>
 </html>
