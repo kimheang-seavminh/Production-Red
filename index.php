9d72442880c2d4e5a08fb45a56e8c28269740c9e
kimheang-seavminh <kimheang@niph.org.kh>
2025-07-14 14:33:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $servername = "192.168.20.13";
 $username = "niph";
 $password = "a0b310084c8ab413ace944be94008be5a0704d18";
- $port="31476";
+ $port="3306";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
