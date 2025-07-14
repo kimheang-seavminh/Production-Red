3fcbbefd0da336c123b0de5fb8a75110b79fa08a
kimheang-seavminh <kimheang@niph.org.kh>
2025-07-14 14:37:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
-$servername = "192.168.20.13";
-$username = "niph";
-$password = "a0b310084c8ab413ace944be94008be5a0704d18";
- $port="3306";
+    // Begin Vault (this is in a vault, not actually hard-coded)
+    $host="192.168.20.13";
+    $username="niph";
+    $password="a0b310084c8ab413ace944be94008be5a0704d18";
+    $dbname="mydb";
+    $port="3306";
+    // End Vault
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+    try {
+        $dbh = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "I am connected.<br/>";
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+        // ... continue with your code
+
+        // PDO closes connection at end of script
+    } catch (PDOException $e) {
+        echo 'PDO Exception: ' . $e->getMessage();
+        exit();
+    }
 ?>
