43f75372e826aa835f8fee1e273de682a9c6100b
kimheang-seavminh <kimheang@niph.org.kh>
2025-07-16 09:22:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     $conn = "";
     // End Vault
 try{ 
-    $conn=mysqli_connect($db_server,
+    $conn = mysqli_connect($db_server,
                         $db_user,
                         $db_pass,
                         $db_name,
