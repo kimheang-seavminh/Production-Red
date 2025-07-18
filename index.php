43baaa5fe6e8a11fd1f6f945f47c5336ddfe9c58
kimheang-seavminh <kimheang@niph.org.kh>
2025-07-18 10:33:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,3 +14,24 @@ try {
   echo "Connection failed: " . $e->getMessage();
 }
 ?>
+
+
+
+<?php
+$host = '192.168.20.12'; // your DB host
+$db   = 'dms_db';      // your DB name
+$user = 'kimheang';    // your DB username
+$pass = 'Seavminh@2024'; // your DB password
+$port = '32364';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $port);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
