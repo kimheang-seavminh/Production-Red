f9b2009771301f4cf5102ecc1a71731c09201f01
kimheang-seavminh <kimheang@niph.org.kh>
2025-07-18 10:35:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 <?php
-$servername = "192.168.20.12";
-$port = "32364";
-$username = "kimheang";
-$password = "Seavminh@2024";
-$database = "dms_db";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password, $username, $database, $port);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
-?>
-
-
-
-<?php
 $host = '192.168.20.12'; // your DB host
 $db   = 'dms_db';      // your DB name
 $user = 'kimheang';    // your DB username
